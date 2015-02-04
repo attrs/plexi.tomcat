3bcaf3e4350d3df1ccfe1388859a83958f9ceae9
joje <joje.attrs@gmail.com>
2015-02-04 18:04:55 +0900

    ..

--- src/activator.js
+++ src/activator.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var http = require('http');
+var pkg = require('../package.json');
 
 var startup = function() {
 	console.log('[tomcat] started');
@@ -48,16 +49,17 @@ module.exports = {
 		
 		var httpService = ctx.require('plexi.http');
 		httpService.filter('tomcat', {
-			pattern: ['**/*.jsp', '/servlets/**', '**/*.jspx', '**/*.do', '/WEB-INF/**'],
+			pattern: ['**/*.jsp', '/servlets/**', '**/*.jspx', '/WEB-INF/**'],
 			filter: function(req, res, next) {
 				if( !req.docbase ) return next(new Error('[tomcat] req.docbase required'));				
-				if( ~req.path.toLowerCase().indexOf('/web-inf/') ) return res.sendStatus(404);
+				if( req.path.toLowerCase().indexOf('/web-inf/') === 0 ) return res.sendStatus(404);
 				
-				var first;
 				var context = getContext(req.docbase);
-				if( !context ) {
-					first = true;
-					context = createContext(req.docbase);
+				if( !context ) context = createContext(req.docbase);
+				
+				var clientcookie = [];
+				for(var k in req.cookies) {
+					clientcookie.push(k + '=' + req.cookies[k]);
 				}
 				
 				var exec = function() {
@@ -65,7 +67,10 @@ module.exports = {
 						hostname: 'localhost',
 						port: 28080,
 						path: context.path + req.url,
-						method: req.method
+						method: req.method,
+						headers: {
+							Cookie: clientcookie.join('; ')
+						}
 					}, function(response) {
 						//console.log('URL', context.path + req.url);
 						//console.log('STATUS: ' + response.statusCode);
@@ -73,10 +78,23 @@ module.exports = {
 						
 						res.statusCode = response.statusCode;
 						response.setEncoding('utf8');
+						if( response.headers['content-type'] ) res.setHeader('Content-Type', response.headers['content-type']);
 						
 						//res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true })
-						var cookie = response.headers['set-cookie'];
-						if( cookie ) res.setHeader('Set-Cookie', cookie);
+						var cookies = response.headers['set-cookie'];
+						if( typeof cookies === 'string' ) cookies = [cookies];
+						if( cookies ) {
+							var cookiearg = [];
+							cookies.forEach(function(cookie) {
+								cookiearg.push(cookie.split('Path=' + context.path).join('Path='));
+							});
+							res.setHeader('Set-Cookie', cookiearg);
+						}
+						
+						var poweredby = (response.headers['x-powered-by'] || '').split(',');
+						poweredby.push(res.getHeader('X-Powered-By') || 'plexi');
+						poweredby.push(pkg.name + '@' + pkg.version);
+						res.setHeader('X-Powered-By', poweredby.join(', '));
 						
 						var payload = '';
 						response.on('data', function (chunk) {
@@ -94,15 +112,30 @@ module.exports = {
 					request.end();
 				};
 				
-				if( first ) {
-					setTimeout(function() {
-						exec();
-					}, 1500);
-				} else {
-					exec();
-				}
+				exec();
 			}
 		});
+		
+		return {
+			startup: function() {
+				return startup;
+			},
+			shutdown: function() {
+				return shutdown();
+			},
+			contexts: function() {
+				return contexts;
+			},
+			createContext: function(docbase, options) {
+				return createContext(docbase, options);
+			},
+			getContext: function(docbase) {
+				return getContext(docbase);
+			},
+			removeContext: function(docbase) 
+				return removeContext(docbase);
+			}
+		};
 	},
 	stop: function(ctx) {
 		shutdown();
