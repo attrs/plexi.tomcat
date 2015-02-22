5e01db0eb8dfe41f339734e46ea5d708af2b0031
joje <joje.attrs@gmail.com>
2015-02-23 03:47:03 +0900

    ..

--- src/install.js
+++ src/install.js
@@ -5,36 +5,29 @@ var fs = require('fs');
 var http = require('http');
 var chalk = require('chalk');
 var ini = require('ini');
+var osenv = require("osenv");
 var inquirer = require("inquirer");
 
 function start() {
-	var detected_javahome = ['/java/jdk6'];
+	var detected_javahome = [];
 	var needjavahome = false;
 		
-	if( process.platform.indexOf('win') === 0 && !(process.env['JAVA_HOME'] || process.env['JRE_HOME'])) {		
-		// C:\Program Files\Java
-		var programfiles_java = path.resolve(osenv.home(), '/Program Files', 'Java');
-		console.log('path', programfiles_java);
-		if( fs.existsSync(programfiles_java) ) {
-			var files = fs.readdirSync(this.PLUGIN_DIR);
+	if( process.platform.indexOf('win') === 0 && !(process.env['JAVA_HOME'] || process.env['JRE_HOME'])) {
+		needjavahome = true;		
+		[path.resolve(osenv.home(), '/Program Files', 'Java'), path.resolve(osenv.home(), '/Program Files (x86)', 'Java')].forEach(function(javadir) {
+			if( fs.existsSync(javadir) ) {
+				var files = fs.readdirSync(javadir);
 
-			for(var i=0; i < files.length; i++) {
-				var dirname = files[i];
-		
-				if( dirname.startsWith('-') || dirname.startsWith('.') ) continue;
-
-				var dir = path.resolve(this.PLUGIN_DIR, dirname);
-				if( fs.statSync(dir).isDirectory() ) {
-					var descriptor = new PluginDescriptor(this, dir);
-					if( this.plugins.exists(descriptor.name, descriptor.version) ) {
-						util.warn(descriptor, 'already exists', dir);
-					} else {
-						this.plugins.add(descriptor.instantiate());
-					}
+				for(var i=0; i < files.length; i++) {
+					var dirname = files[i];
+					if( dirname.toLowerCase().indexOf('jdk') === 0 || dirname.toLowerCase().indexOf('jre') === 0 ) {					
+						var dir = path.resolve(javadir, dirname);
+						detected_javahome.push(dir);
+					}				
 				}
 			}
-		}
-	}		
+		});
+	}
 	
 	inquirer.prompt([
 		{
@@ -56,16 +49,65 @@ function start() {
 				if( !value.javahome && needjavahome ) return true;
 			},
 			validate: function(value) {
-				if( value ) return true;
+				if( fs.existsSync(value) ) return true;
 			}
 		}, {
 			type: "list",
 			name: "version",
 			message: "Tomcat Version",
-			choices: [ "6.0.43", "7.0.59", "8.0.18" ]
+			choices: [ "6.0.43", "7.0.59", "8.0.18" ],
+			filter: function(value) {
+				return value[0];
+			}
 		}
 	], function( answers ) {
-		console.log( JSON.stringify(answers, null, "  ") );
+		var url;
+		var version = answers.version;
+
+		if( version === '6' ) {
+		} else if() {
+		}
+
+		console.dir(answers);
+
+		return true;
+
+
+		var url = geturl(version);
+		var filename = url.substring(url.lastIndexOf('/') + 1);
+
+		// check cache, if file exists in cache, use it
+		var userhome = osenv.home();
+		var cachedir = path.resolve(userhome, '.plexi.wordpress');
+		var cachefile = path.resolve(cachedir, filename);
+		var dest = path.resolve(__dirname, '..', 'wordpress');
+		if( !fs.existsSync(cachedir) ) {
+			try {
+				fs.mkdirSync(cachedir);
+			} catch(err) {
+				cachedir = path.resolve(__dirname, '..', 'download');
+				cachefile = path.resolve(cachedir, filename);
+			}
+		}
+
+		if( !fs.existsSync(cachefile) ) {
+			new Download({ extract: true, strip: 1, mode: '755' })
+				.get(url)
+				.dest(cachefile)
+				.use(function(instance, url) {
+					process.stdout.write(chalk.green('Download\n'));
+				})
+				.use(progress())
+				.run(function (err, files, stream) {
+					if (err) {
+						process.stdout.write(chalk.red('Error\n'));
+						console.log(chalk.red(err));
+						return task_install();
+					}
+					install();
+				}
+			);
+		}
 	});
 };
 
@@ -81,22 +123,6 @@ function geturl(version) {
 	*/
 	return (!version || version === 'latest') ? 'http://wordpress.org/latest.tar.gz' : 'http://wordpress.org/wordpress-' + version + '.tar.gz';
 }
-
-function rmdirRecursive(path) {
-    var files = [];
-    if( fs.existsSync(path) ) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-            var curPath = path + "/" + file;
-            if(fs.lstatSync(curPath).isDirectory()) { // recurse
-                rmdirRecursive(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(path);
-    }
-};
 
 var task_install = function () {
 	process.stdin.resume();
